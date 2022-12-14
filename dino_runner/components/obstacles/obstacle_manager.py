01090590cf618f3f240d4ec1cbf5c31e4ac8e83b
EuRic7 <120383860+EuRic7@users.noreply.github.com>
2022-12-14 07:06:17 -0300

    Adicionado Cacto Grande + Bird

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,8 +1,10 @@
+import random
+
 import pygame
 
 from dino_runner.utils.constants import SMALL_CACTUS
 from dino_runner.components.obstacles.cactus import Cactus
-
+from dino_runner.components.obstacles.bird import Bird
 
 
 class ObstacleManager:
@@ -10,8 +12,15 @@ class ObstacleManager:
         self.obstacles = []
 
     def update(self, game):
+        obstacle_type = [
+            Cactus(),
+            Bird(),
+        ]
+
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            self.obstacles.append(
+                obstacle_type[random.randint(0, 1)]
+            )
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
